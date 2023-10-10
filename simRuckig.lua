23b16dd45089966d77a3493bc7ae92ac8aef0268
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-10 16:56:43 +0200

    lua-format

--- simRuckig.lua
+++ simRuckig.lua
@@ -1,5 +1,5 @@
 -- For now, the Ruckig functionality goes through CoppeliaSim functions, i.e. sim.ruckigPos, etc.
 
-local simRuckig=loadPlugin('simRuckig')
+local simRuckig = loadPlugin('simRuckig');
 
 return simRuckig
